37884af64fea4e7e9df1f6c663703e38bf458d52
John Erick Santos <johnsantosdev@gmail.com>
2023-05-01 05:19:24 +1000

    refactor: create generic onboarding view controller

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    let heroImageName: String
+    let titleText: String
+
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
@@ -17,24 +20,38 @@ class OnboardingViewController: UIViewController {
         style()
         layout()
     }
+
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension OnboardingViewController {
     func style() {
+        view.backgroundColor = .systemBackground
+
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
 
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
 
     func layout() {
