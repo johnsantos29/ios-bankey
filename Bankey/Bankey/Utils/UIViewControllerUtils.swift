b7b2e326a9b5d9cf5abf93e9aaa764cdd8237e40
John Erick Santos <johnsantosdev@gmail.com>
2023-05-03 05:17:24 +1000

    feat: Add tab view controller

---
+++ Bankey/Bankey/Utils/UIViewControllerUtils.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewControllerUtils.swift
+//  Bankey
+//
+//  Created by John Erick Santos on 3/5/2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let statusBarSize = UIApplication.shared.statusBarFrame.size
+        let frame = CGRect(origin: .zero, size: statusBarSize)
+        let statusBarView = UIView(frame: frame)
+
+        statusBarView.backgroundColor = appColor
+        view.addSubview(statusBarView)
+    }
+
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
